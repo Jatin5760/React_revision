7e9ef5622b3d7e1a77413762ac43fd32240ec6d5
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-15 01:26:36 +0530

    Commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,15 @@
-import Chai from "./Chai"
+import Chai from "./Chai";
 function App() {
+  const userName = "Jatin Sharmaaaa";
   return (
-    // This is fragment wala concept, we can return multiple elements here
+    // This is fragment wala concept, we can return multiple elements here (JSX hai ye)
+
+    // Niche {} curly brackets mai final outcome ayega JS ka iske andar ham koi bhi expression nhi likh sakte like forloop aur if else, isme sirf evalated expression hi likh sakte hai.
     <>
-      <h1>Chai aur react with vite | Jatin Sharma</h1>
-      <Chai/>
+      <h1>Chai aur react with vite | {userName}</h1>
+      <Chai />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,98 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// function MyApp(){
+//     return (
+//         <div>
+//             <h1>
+//                 Custom App!
+//             </h1>
+//         </div>
+//     )
+// }
+
+// Jo MyApp mai ham likhte hai wo at the end of the day niche jaise object mai parse hojata hai
+// Agar reactElement ko direct hi render kardenge toh run hona chaiye, but lets see
+
+
+
+
+
+/*
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+*/
+
+
+
+
+
+// const anotherElement = (
+//     <a href='https://google.com' target='_blank'>Visit Google</a>
+// )
+
+
+
+
+
+// Jab sara tree ban jata hai uske baad varaible injection hota hai, aur ye sidha ke sidha object ke andar inject ho jata hai.
+const anotherUser = "Jayant"
+
+const ReactElement3 = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank'},
+    'click me to visit Google',
+    // Yaha par variable ayese directly inject ho jata hai, aur yaha ham koi if else, ya loop nhi daal sakte kyonki syntactically valid nahi hai.
+    anotherUser
+)
+
+
+
+// Can't use if else expression inside the object
+
+// {
+//     if()
+// }
+
+
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    //<App />
+
+    // Ye jo MyApp hai wo bas ek function hai aur transpiled hogya hai bas
+    // Ye function ki trah bhi execute ho sakta hai, But karnege nahi
+
+    // Myapp()
+
+    //<MyApp/>
+
+    // Object thodi ayese execute hoga, function hona jaruri hai
+    //<ReactElement /> 
+
+    // Ye isley nahi chal raha hai because may be iska render function jo likha ho uske methods ke name alag ho..
+    //ReactElement
+
+
+    // Yaha par ye code chal gya kyonki hamne function likha tha jo ki react ne internaly convert karldiya isko tree mai ek object ki tarah.
+    //anotherElement
+
+
+    // Ye isley run hua because hamne react ke according hi element ko likha hai
+    ReactElement3
+
+
+    //<App />
+
 )
