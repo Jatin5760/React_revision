28d5108361230ea21b8b81fd268a8204a04a92b4
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-15 18:48:56 +0530

    Commit

---
+++ 08integratecss/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Laptop from './Components/Laptop'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Laptop name = "HP" model = "Probook" price = {75000}/>
+        <Laptop name = "Lenevo" model = "Yoga" price = {50000}/>
+        <Laptop name = "Apple" model = "Macbook-Air-M1" price = {95000}/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 08integratecss/src/Components/Laptop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Laptop = ({name, model, price}) => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Laptop
