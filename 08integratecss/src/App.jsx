809421ea3da4e6bc31e4d6175bf558231186a146
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-15 19:18:31 +0530

    Commit

--- 08integratecss/src/App.jsx
+++ 08integratecss/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Laptop from './Components/Laptop'
+import React from "react";
+import Laptop from "./Components/Laptop";
 
 const App = () => {
   return (
     <>
       <div>
-        <Laptop name = "HP" model = "Probook" price = {75000}/>
-        <Laptop name = "Lenevo" model = "Yoga" price = {50000}/>
-        <Laptop name = "Apple" model = "Macbook-Air-M1" price = {95000}/>
+        <Laptop name="HP" model="Probook" price={75000} />
+        <Laptop name="Lenevo" model="Yoga" price={50000} />
+        <Laptop name="Apple" model="Macbook-Air-M1" price={95000} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- 08integratecss/src/Components/Laptop.jsx
+++ 08integratecss/src/Components/Laptop.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./laptop.css";
 
-const Laptop = ({name, model, price}) => {
+const Laptop = ({ name, model, price }) => {
+  // Isme suggestion nahi milta hai
+  const obj = {
+    backgroundColor: "blue",
+    color: "white",
+    margin: "20px",
+    borderRadius: "10px",
+    padding: "10px",
+    border: "2px solid white",
+  };
+
   return (
     <>
-      
-    </>
-  )
-}
+      <div
+        // Inline styling
+        // style={{
+        //   backgroundColor: "yellow",
+        //   color: "black",
+        //   margin: "20px",
+        //   borderRadius: "10px",
+        //   padding: "10px",
+        //   border: "2px solid white",
+        // }}
 
-export default Laptop
+        // Internal CSS
+        style = {obj}
+
+        // External Css
+        //className="extcss"
+      >
+        <h3>Brand Name: {name}</h3>
+        <h3>Model Name: {model}</h3>
+        <h3>Price: {price}</h3>
+
+        <br />
+      </div>
+    </>
+  );
+};
+
+export default Laptop;
