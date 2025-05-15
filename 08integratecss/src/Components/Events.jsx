b750cd82b7c7f7da2ffdeb6e7089500b6c369498
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-15 20:43:18 +0530

    Commit

--- 08integratecss/src/Components/Events.jsx
+++ 08integratecss/src/Components/Events.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import "./Events.css"
+import React from "react";
+import "./Events.css";
 
 const Events = () => {
+  const handleClick = () => {
+    alert("Button is Clicked");
+  };
+
+  // Jab function parameter leta hai toh usko call back mai call karna hota hai
+  const additon = (a) => {
+    alert(a + 10);
+  };
   return (
     <div>
       <h1> We are learning events</h1>
-    </div>
-  )
-}
+      <button onClick={handleClick}>Click Me</button>
+      <button onMouseOver={handleClick}>Hover Me</button>
 
-export default Events
+      {/* Yaha callback function use kara hamne because function parameterized tha */}
+
+      <button onClick={() => additon(100)}>Calculate Me</button>
+    </div>
+  );
+};
+
+export default Events;
