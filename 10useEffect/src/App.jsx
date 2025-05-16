e0bcfb7f1b65a44fdf809676845bb97e4148cd12
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-16 22:02:05 +0530

    Commit

--- 10useEffect/src/App.jsx
+++ 10useEffect/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 //import UseEffect from "./Components/UseEffect";
-import Fetch_Data_Api from "./Components/Fetch_Data_Api";
+//import Fetch_Data_Api from "./Components/Fetch_Data_Api";
+import Forms from "./Components/Forms";
 
 function App() {
   return (
     <>
       {/* <UseEffect/> */}
-      <Fetch_Data_Api />
+
+      {/* <Fetch_Data_Api /> */}
+
+      <Forms />
     </>
   );
 }
