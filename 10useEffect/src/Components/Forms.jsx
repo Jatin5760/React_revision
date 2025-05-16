ffd1257306638d781c0ec54a0d1f3dffa9960c4e
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-16 22:40:41 +0530

    Commit

--- 10useEffect/src/Components/Forms.jsx
+++ 10useEffect/src/Components/Forms.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./Forms.css";
+import { useState } from "react";
 
 const Forms = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
   return (
     <div>
       <h1>Forms</h1>
-    </div>
-  )
-}
+      <form>
+        <div>
+          Name : <input value={name}
+          onChange={(e)=> setName(e.target.value)} 
+          type="text" />
+        </div>
 
-export default Forms
+        <br />
+
+        <div>
+          Email : <input value={email} 
+          onChange={(e)=> setEmail(e.target.value)} 
+          type="email" />
+        </div>
+
+        <br />
+
+        <div>
+          Password : <input value={password} 
+          onChange={(e)=> setPassword(e.target.value)} 
+          type="password" />
+        </div>
+
+        <br />
+
+        <div>
+          <button onClick={()=>{}}>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Forms;
