1df050c5d89649e905b43835b843362027fc1d81
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-18 18:15:22 +0530

    Commit

--- 11mealRecipe/src/Components/Meal.jsx
+++ 11mealRecipe/src/Components/Meal.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Meal = () => {
-  return (
-    <div>
-      hii
-    </div>
-  )
-}
+    
+    const [meal, setMeal] = useState(second)
+  
 
-export default Meal
+    useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      const api = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian`
+      );
+
+      const data = await api.json();
+      console.log(data);
+    };
+
+    fetchDataFromAPI();
+  }, []);
+
+  return <div>hii</div>;
+};
+
+export default Meal;
