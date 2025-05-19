4695b9f421d4ed41cdfe766cbd6e977c6f1f9640
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-19 16:30:03 +0530

    Commit

--- 12reactRouter/src/App.jsx
+++ 12reactRouter/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+import Team from './Pages/Team'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +14,11 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route>
-            
-          </Route>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/home' element={<About/>}></Route>
+          <Route path='/home' element={<Contact/>}></Route>
+          <Route path='/home' element={<Product/>}></Route>
+          <Route path='/home' element={<Team/>}></Route>
         </Routes>
       </Router>
     </>
