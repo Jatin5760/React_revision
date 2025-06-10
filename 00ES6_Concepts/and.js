3759636459b36117f33db37563531443c0867717
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-10 18:44:20 +0530

    Commit

---
+++ 00ES6_Concepts/and.js
@@ -0,0 +1,11 @@
+function getName(name){
+    return name;
+}
+
+let a = false;
+let b = true
+
+console.log(a&& getName("Jatin Sharma"));
+
+// Here if the a is false then it won't check the next conditon, directly print the false;
+
