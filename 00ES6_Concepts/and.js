42f0d896ef5f795e44f6af1899eef2a14107dc42
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-10 18:46:57 +0530

    Commit

--- 00ES6_Concepts/and.js
+++ 00ES6_Concepts/and.js
@@ -3,9 +3,18 @@ function getName(name){
 }
 
 let a = false;
-let b = true
+let b = true;
 
-console.log(a&& getName("Jatin Sharma"));
+let c = false;
+let d = true;
 
 // Here if the a is false then it won't check the next conditon, directly print the false;
+console.log(a && getName("Jatin Sharma"));
+
+
+// If we use or here and if c is false then also it will check the second parameter.
+console.log(c || getName("Jayant Sharma"));
+
+
+
 
