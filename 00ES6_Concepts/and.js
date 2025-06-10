27063c8be7db1d8f18956dda1207d8e8e8966ac2
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-10 18:54:28 +0530

    Commit

--- 00ES6_Concepts/and.js
+++ 00ES6_Concepts/and.js
@@ -19,7 +19,6 @@ console.log(c || getName("Jayant Sharma"));
 
 
 // Tempelate literals
-
 // This is the older version
 let name1 = 'John';
 let name2 = 'Doe';
@@ -29,12 +28,16 @@ console.log(name1+ " "+name2);
 console.log(`First name: ${name1} and Second name: ${name2}`);
 
 
+
+
+
 // Ternary Operator
 // Condition ? statement1 : statement2
-let showRecipeOne = false;
-function getRecipeOneName(recipeName){
-    return recipeName;
-}
+
+let age = 2;
+age>=20? console.log(`Your age is greater than 20`): console.log(`Your age is less than 20`);
+
+
 
 
 
