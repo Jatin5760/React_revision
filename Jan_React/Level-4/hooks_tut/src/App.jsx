61cdec1ca3a6ad3d8c181e934e36a2e3ec327e97
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-24 01:14:51 +0530

    commit

--- Jan_React/Level-4/hooks_tut/src/App.jsx
+++ Jan_React/Level-4/hooks_tut/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
   let [count, setCount] = useState(0);
   let [color, setColor] = useState("blue");  
+  useEffect(()=>{
+    setTimeout(()=>{
+      setCount(++count)
+    }, 2000)
+  }, )
   return (
     <>
       <h1>My Favourite color {color} </h1>
