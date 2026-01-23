7347eaaba2ac3143221c614d6bd4617bb5fe9b2c
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 21:25:31 +0530

    commit

---
+++ Jan_React/Level-3/props/src/components/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function Box(props) {
+    console.log(props);
+    
+  return (
+    <div className='box'>
+        <h1>Jatin Sharma</h1>
+        <h2>Web Developer</h2>
+    </div>
+  )
+}
+
+export default Box
