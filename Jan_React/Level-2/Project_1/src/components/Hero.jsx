639cfba4d052447a04722a1b59f2648c5268aeed
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 20:01:15 +0530

    commit

--- Jan_React/Level-2/Project_1/src/components/Hero.jsx
+++ Jan_React/Level-2/Project_1/src/components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Hero() {
   return (
     <div className="hero">
-        <img src="https://pixabay.com/images/download/woman-8125244_1920.jpg" alt="" />
-        {/* <img src="" alt="" />
-        <img src="" alt="" /> */}
+        <img src="https://images.unsplash.com/photo-1764946315082-951959a86c12?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero" style={{width: "300px"}} />
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=800" alt="Hero" style={{width: "300px"}} />
+        <img src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=800" alt="Hero" style={{width: "300px"}} />
     </div>
   )
 }
