3b241b81c1c83c982bc62c1f3d36251dfc3241bb
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 18:33:52 +0530

    Commit

---
+++ Jan_React/script.js
@@ -0,0 +1,10 @@
+// let h1 = document.createElement("h1")
+// h1.innerHTML = "Jatin Sharma";
+
+// document.getElementById("root").appendChild(h1)
+
+
+// Using React
+
+let h1 = React.createElement("h1",null,"Jatin Sharma using react")
+ReactDOM.createRoot(document.getElementById("root")).render(h1)
