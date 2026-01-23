5713913aea8fc5e9e2999b002352c08a0d5ca0c3
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-24 01:40:39 +0530

    Commit

--- Jan_React/Level-4/hooks_tut/src/App.jsx
+++ Jan_React/Level-4/hooks_tut/src/App.jsx
@@ -1,22 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css'
 
 function App() {
-  let [count, setCount] = useState(0);
-  let [color, setColor] = useState("blue"); 
-   
+  // let [count, setCount] = useState(0);
+  // let [color, setColor] = useState("blue"); 
+
+  // useEffect(()=>{
+  //   setTimeout(()=>{
+  //     setCount(++count)
+  //   }, 2000)
+  // }, [])
+
+  let count = useRef(0); // useRef returns an object with "current" varible.
+  // console.log(count.current);
+  let [num, setNum] = useState(0);
+
   useEffect(()=>{
-    setTimeout(()=>{
-      setCount(++count)
-    }, 2000)
-  }, )
+    count.current = count.current+1;
+  })
 
   return (
     <>
-      <h1>My Favourite color {color} </h1>
-      <button onClick={()=>setColor("black")}>Black</button>
-      <h2>{count}</h2>
-      <button onClick={()=>setCount(count = count + 1)}>Increase</button>
+      {/* <h1>My Favourite color {color} </h1>
+      <button onClick={()=>setColor("black")}>Black</button> */}
+
+      <h1>{count.current}</h1>
+      <h1>{num}</h1>
+      <button onClick={()=>setNum(++num)}>Increase</button>
     </>
   )
 }
