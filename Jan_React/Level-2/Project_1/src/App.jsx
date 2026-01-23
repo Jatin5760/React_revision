393820d25bb5c5ea3b8d66e7aff49601fb72f3bc
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 19:35:14 +0530

    commit

--- Jan_React/Level-2/Project_1/src/App.jsx
+++ Jan_React/Level-2/Project_1/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
     <>
-      
+      <Nav/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
