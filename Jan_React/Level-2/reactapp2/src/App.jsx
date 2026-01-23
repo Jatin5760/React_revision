049e15fc355029a9a3d58a403f7443db545879f9
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 19:22:51 +0530

    commit

--- Jan_React/Level-2/reactapp2/src/App.jsx
+++ Jan_React/Level-2/reactapp2/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 
 function App() {
+  let name = "Jatin";
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello {name}!</h1>
+      <div className="box">
+        
+      </div>
     </>
   )
 }
