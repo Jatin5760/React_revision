f5c44c8e23fe62a84958a9f5500f8ad270116e84
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 23:15:36 +0530

    Commit

--- Jan_React/Level-3/project_2/src/App.jsx
+++ Jan_React/Level-3/project_2/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Nav from './components/Nav/Nav'
+import Hero from "./components/Hero/Hero"
+import Card from "./components/Card/Card"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   
 
   return (
     <>
-      
+      <Nav/>
+      <Hero/>
+      <Card/>
+      <Footer/>
     </>
   )
 }
