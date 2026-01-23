52875aa523333941465460b9d25f0f6ccddcbc2a
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-23 19:50:08 +0530

    commit

---
+++ Jan_React/Level-2/Project_1/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="hero">
+        <img src="https://pixabay.com/images/download/woman-8125244_1920.jpg" alt="" />
+        {/* <img src="" alt="" />
+        <img src="" alt="" /> */}
+    </div>
+  )
+}
+
+export default Hero
---
+++ Jan_React/Level-2/Project_1/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+function nav() {
+  return (
+    <nav>
+        <h1>Logo</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default nav
