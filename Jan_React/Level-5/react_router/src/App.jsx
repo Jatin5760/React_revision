6bb06d92b2476b6bdf8c903593bf5c45441c50bf
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-24 10:50:04 +0530

    commit

--- Jan_React/Level-5/react_router/src/App.jsx
+++ Jan_React/Level-5/react_router/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
-
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import {Outlet} from 'react-router-dom'
 function App() {
   return (
     <>
-      
+      <Nav/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
