b9c3c5da4ff0be8f275b433f182ea705f3bbb86d
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-24 10:31:49 +0530

    Commit

--- Jan_React/Level-4/hooks_tut/src/App.jsx
+++ Jan_React/Level-4/hooks_tut/src/App.jsx
@@ -10,20 +10,24 @@ function App() {
   //     setCount(++count)
   //   }, 2000)
   // }, [])
-
+  let inputvalue = useRef("");
   let count = useRef(0); // useRef returns an object with "current" varible.
+  // useRef will persist re rendering and it will not allow us to do unnessary render things
+  // useRef is also used to access DOM elements.
+
   // console.log(count.current);
   let [num, setNum] = useState(0);
 
   useEffect(()=>{
     count.current = count.current+1;
+
   })
 
   return (
     <>
       {/* <h1>My Favourite color {color} </h1>
       <button onClick={()=>setColor("black")}>Black</button> */}
-
+        <input type="text" ref={inputvalue} value="Jatin Sharma"/>
       <h1>{count.current}</h1>
       <h1>{num}</h1>
       <button onClick={()=>setNum(++num)}>Increase</button>
