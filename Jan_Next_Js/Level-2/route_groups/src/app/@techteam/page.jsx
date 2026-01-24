6d36a17d76dc60ce8ca07f1c25c0af37929d6d36
Jatin5760 <jatinsharma14202003@gmail.com>
2026-01-24 18:54:45 +0530

    Commit

---
+++ Jan_Next_Js/Level-2/route_groups/src/app/@techteam/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Tech team page</div>
+  )
+}
+
+export default page
--- Jan_Next_Js/Level-2/route_groups/src/app/layout.js
+++ Jan_Next_Js/Level-2/route_groups/src/app/layout.js
@@ -1,12 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-export default function RootLayout({ children, team }) {
+export default function RootLayout({ children, team, techteam }) {
   return (
     <html lang="en">
       <body className="w-screen h-screen flex">
        <div className="w-[50%] bg-amber-950 text-white" >{children}</div>
        <div className="w-[50%] bg-amber-300 text-black">{team}</div>
+       <div className="w-[50%] bg-amber-600 text-black">{techteam}</div>
       </body>
     </html>
   );
