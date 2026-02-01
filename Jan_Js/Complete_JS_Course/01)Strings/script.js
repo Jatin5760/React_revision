6fac6de90a37d5ef1d7626245d4cd16e3c60c360
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-01 22:46:23 +0530

    Commit

---
+++ Jan_Js/Complete_JS_Course/01)Strings/script.js
@@ -0,0 +1,23 @@
+const message = "       Hello ";
+console.log(message[3]);
+console.log(message.length);
+
+// Property
+/*
+    1) length
+*/
+
+// Methods - String ka method string ko modify nhi karta bas ek naya result hamko de deta hai.
+/*
+    1) toUpperCase()
+    2) toLowerCase()
+    3) trim()
+    4) trimStart()
+    5) trimEnd()
+*/
+
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.trim());
+console.log(message.trimStart());
+console.log(message.trimEnd());
