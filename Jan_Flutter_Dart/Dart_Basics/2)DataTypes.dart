1143e417a98651d5731f741de1d5de7738580cac
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-02 21:14:59 +0530

    Commit

--- Jan_Flutter_Dart/Dart_Basics/2)DataTypes.dart
+++ Jan_Flutter_Dart/Dart_Basics/2)DataTypes.dart
@@ -13,3 +13,29 @@
   2) List   [1,2,3]
   3) Map    {'key':'value'}
  */
+
+
+// Keywords
+/*
+  1) final  - after first initialization not changable in this lifecybcle
+  2) const  - set while compiling not changeble
+  3) var  - type is assigned automatically
+  4) late - to say we will assign the variable a value later on.
+*/
+
+
+
+void main(){
+  bool boolOutput = false;
+  print(boolOutput);
+
+  int intOutput = -1;
+  print(intOutput);
+
+  double doubleOutput = -3.69;
+  print(doubleOutput);
+
+
+  String stringOutput = 'text';
+  print(stringOutput);
+}
