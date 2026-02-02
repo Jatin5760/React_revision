98fa39be9a218892609a93c69d5cdd22807d1cc8
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-02 21:03:10 +0530

    Commit

--- Jan_Flutter_Dart/Dart_Basics/1.dart
+++ Jan_Flutter_Dart/Dart_Basics/1.dart
@@ -1,6 +1,14 @@
 void main(){
 
-  int outputInt = 2;
-  
+  int outputInt = 2; // Initialization
+
   print('Output : $outputInt');
+
+  late int lateOutputInt; // declaration
+
+  lateOutputInt = 42; // allocation
+  
+  print('lateOutputInt: $lateOutputInt');
+
+
 }
---
+++ Jan_Flutter_Dart/Dart_Basics/2)DataTypes.dart
@@ -0,0 +1,15 @@
+// Primitive data types
+
+/*
+  1) bool     true/false
+  2) integer  3
+  3) double   4.7
+ */
+
+// Complex data types
+
+/*
+  1) String 'text'
+  2) List   [1,2,3]
+  3) Map    {'key':'value'}
+ */
