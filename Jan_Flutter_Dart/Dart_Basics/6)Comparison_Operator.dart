04535969b325b283acb29c5b00fa2b1cc4fe3607
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-05 00:11:39 +0530

    Commit

---
+++ Jan_Flutter_Dart/Dart_Basics/6)Comparison_Operator.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  int age = 16;
+
+  print('user is $age years old.');
+
+  if(age >= 18){
+    print("You can watch the movie");
+  }
+  else{
+    print("You can't watch the movie");
+  }
+}
