95196944e58e537a5dd91455c45ebb033442416a
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-07 00:38:28 +0530

    Commit

---
+++ Jan_Flutter_Dart/Dart_Basics/10)Async.dart
@@ -0,0 +1,9 @@
+/*
+  Asynchronous programming
+  Allows us to write the program in which the tasks are no longer solved
+  one after the other and instead solved in parallel.
+
+  While one task is being processed, processes are already running on another task.
+
+  Also, some tasks take more time - with async we can wait for them to finish.
+ */
