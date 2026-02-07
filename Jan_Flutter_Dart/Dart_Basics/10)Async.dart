9bf1c5ae67a2d4d6ab1e6c98228538d6e7ddf612
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-07 17:41:57 +0530

    Commit

--- Jan_Flutter_Dart/Dart_Basics/10)Async.dart
+++ Jan_Flutter_Dart/Dart_Basics/10)Async.dart
@@ -9,3 +9,43 @@
  */
 
 // async/await - wait for an event to occur in a thread/execution thread.
+
+void main() async {
+  DataService dataService = DataService();
+  String data = await dataService.getData();
+  print(data);
+}
+
+class DataService {
+  Future<String> _getDataFromCloud() async {
+    // get data from the cloud -> time
+    await Future.delayed(Duration(seconds: 4));
+    print("Get data finished");
+    return "Fake Data";
+  }
+
+  Future<String> _parseDataFromCloud({required String dataFromCloud}) async {
+    // get data from the cloud -> time
+    await Future.delayed(Duration(seconds: 2));
+    print("Parsed data finished");
+    return "Parsed Data";
+  }
+
+  Future getData() async {
+    // final String dataFromCloud = await _getDataFromCloud();
+    // final String parseData = await _parseDataFromCloud(
+    //   dataFromCloud: dataFromCloud,
+    // );
+
+    // Alternative Syntax
+    final String parsedData = await _getDataFromCloud().then((dataFromCloud) async {
+      return await _parseDataFromCloud(dataFromCloud: dataFromCloud);
+    });
+
+    return parsedData;
+
+    // get data -> time
+    // parse data -> time
+    // return data;
+  }
+}
