e2c435b8ccca4d1417cfdb797d2281c83d07b84d
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-07 21:55:07 +0530

    Commit

---
+++ Jan_Flutter_Dart/Dart_Basics/11)Streams.dart
@@ -0,0 +1,41 @@
+// Streams
+
+/*
+  Future represent a value that they deliver asynchronously.
+
+  Streams are similar but instead of a single value they can return zero or more values or errors over time.
+  
+ */
+
+import 'dart:async';
+
+void main(List<String> args) {
+  final numberGenerator = NumberGenerator();
+
+  StreamSubscription sub1 = numberGenerator.stream.listen((event){
+    print('sub1: $event');
+  });
+  // working with streams
+}
+
+class NumberGenerator {
+  int _counter = 0;
+
+  StreamController<int> _controller = StreamController<int>();
+
+  Stream<int> get stream => _controller.stream;
+
+  NumberGenerator() {
+    final Timer timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      // sends a event over the stream every second.
+      _controller.add(_counter);
+      _counter++;
+    });
+
+    Future.delayed(Duration(seconds: 10), () {
+      // closes the stream and timer after 10 sec.
+      timer.cancel();
+      _controller.close();
+    });
+  }
+}
