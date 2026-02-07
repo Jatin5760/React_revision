73bd679e123b2d28db20f958f03e4dd32d184919
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-07 22:13:24 +0530

    Commit

--- Jan_Flutter_Dart/Dart_Basics/11)Streams.dart
+++ Jan_Flutter_Dart/Dart_Basics/11)Streams.dart
@@ -15,13 +15,17 @@ void main(List<String> args) {
   StreamSubscription sub1 = numberGenerator.stream.listen((event){
     print('sub1: $event');
   });
+
+  StreamSubscription sub2 = numberGenerator.stream.listen((event){
+    print('sub2: ${event * event}');
+  });
   // working with streams
 }
 
 class NumberGenerator {
   int _counter = 0;
 
-  StreamController<int> _controller = StreamController<int>();
+  StreamController<int> _controller = StreamController<int>.broadcast();
 
   Stream<int> get stream => _controller.stream;
 
