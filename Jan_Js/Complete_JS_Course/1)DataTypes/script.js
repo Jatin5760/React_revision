b881ad252884a159abe49b2e82445261d7f2f373
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-21 00:17:11 +0530

    Commit

---
+++ Jan_Js/Complete_JS_Course/1)DataTypes/script.js
@@ -0,0 +1,15 @@
+// Primitive dataype
+
+// Javascript is dynamically typed, we do not manually define the datatype of the value.
+
+
+/*
+    Number
+    String
+    Boolean
+    Undefined
+    Null
+    Symbol
+    Bigint
+*/
+
