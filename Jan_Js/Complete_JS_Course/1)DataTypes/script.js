77689f0ccb8c554d87f89848e0d25cc025dabc71
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-21 11:18:28 +0530

    Commit

--- Jan_Js/Complete_JS_Course/1)DataTypes/script.js
+++ Jan_Js/Complete_JS_Course/1)DataTypes/script.js
@@ -15,10 +15,10 @@
 */
 
 let language = true;
-console.log(language);
+//console.log(language);
 
 
-console.log(typeof null); // Object
+//console.log(typeof null); // Object
 
 
 
@@ -30,3 +30,10 @@ age = 31;
 
 const birthYear = 1991;
 // birthYear = 1990; // give error because now this variable is immutable in nature.
+
+
+
+
+
+
+
