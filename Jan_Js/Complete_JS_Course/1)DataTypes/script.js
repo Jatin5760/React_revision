df6cca111e29232bef988e4eb624675f45e2a35d
Jatin5760 <jatinsharma14202003@gmail.com>
2026-02-21 10:58:02 +0530

    Commit

--- Jan_Js/Complete_JS_Course/1)DataTypes/script.js
+++ Jan_Js/Complete_JS_Course/1)DataTypes/script.js
@@ -2,6 +2,7 @@
 
 // Javascript is dynamically typed, we do not manually define the datatype of the value.
 
+// Js code runs from top to bottom
 
 /*
     Number
@@ -13,3 +14,19 @@
     Bigint
 */
 
+let language = true;
+console.log(language);
+
+
+console.log(typeof null); // Object
+
+
+
+// Variables
+
+let age = 30;
+age = 31;
+
+
+const birthYear = 1991;
+// birthYear = 1990; // give error because now this variable is immutable in nature.
